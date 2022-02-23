5b586ef78d7c868e190d95d030952d31f448943f
Jair <52211084+jquirogaassat@users.noreply.github.com>
2022-02-23 10:10:56 -0300

    actualizacion DAL

---
+++ DAL/DALCliente.cs
@@ -0,0 +1,32 @@
+﻿using BE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DALCliente : BE.ICrud<BE.Cliente>
+    {
+        public bool Add(Cliente entAlta)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Cliente entAlta)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Cliente> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Cliente entAlta)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
