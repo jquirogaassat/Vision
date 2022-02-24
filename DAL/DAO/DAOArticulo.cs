4233295a44f81066ab55a966163258aa33d31b41
Jair <52211084+jquirogaassat@users.noreply.github.com>
2022-02-24 13:01:27 -0300

    ACT DAO

--- DAL/DAO/DAOArticulo.cs
+++ DAL/DAO/DAOArticulo.cs
@@ -1,14 +1,60 @@
-﻿using System;
+﻿using BE;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DAL.DAO
 {
-    internal class DAOArticulo
+    internal class DAOArticulo : BE.ICrud<BE.Articulo>
     {
         private string connstring = "";// dir bd
+        private SqlConnection conn;
+        private string spInsert = "AgregarArticulo";// storeprocedure de la bd
+        private string spUpdate = "ActualizarArticulo";// storeprocedure de la bd
+        private string spDelete = "EliminarArticulo"; // storeprocedure de la bd
+        private string spSelectAll = "ArticuloSelectAll"; // storeprocedure de la bd 
+        private DataSet ds;
+        private SqlDataAdapter da;
+        private SqlCommand comm;
 
+        #region SINGLETON
+        private DAOArticulo()
+        {
+
+        }
+
+        private static DAOArticulo instancia;
+        public static DAOArticulo getInstancia()
+        {
+            if (instancia == null)
+            {
+                instancia = new DAOArticulo();
+            }
+            return instancia;
+        } 
+        #endregion
+        public bool Add(Articulo entAlta)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Articulo entAlta)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Articulo> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Articulo entAlta)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
