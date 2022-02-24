7fd1f7ad29a12e412eef56286a0e3cd7562ccfc2
Jair <52211084+jquirogaassat@users.noreply.github.com>
2022-02-24 10:46:08 -0300

    dao

---
+++ DAL/DAO/DAOArticulo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.DAO
+{
+    internal class DAOArticulo
+    {
+        private string connstring = "";// dir bd
+
+    }
+}
