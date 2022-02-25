c11f2d2c87abd6075f14f7fd36d4e1c4113cd032
Jair <52211084+jquirogaassat@users.noreply.github.com>
2022-02-25 12:50:56 -0300

    act dao

--- DAL/DALArticulo.cs
+++ DAL/DALArticulo.cs
@@ -9,6 +9,22 @@ namespace DAL
 {
     public class DALArticulo : BE.ICrud<BE.Articulo>
     {
+
+        #region singleton
+        private DALArticulo()
+        {
+
+        }
+        private static DALArticulo instnacia;
+        public static DALArticulo GetInstnacia()
+        {
+            if (instnacia == null)
+            {
+                instnacia = new DALArticulo();
+            }
+            return instnacia;
+        } 
+        #endregion
         public bool Add(Articulo entAlta)
         {
             throw new NotImplementedException();
---
+++ DAL/DAO/DAOEstadistica.cs
@@ -0,0 +1,60 @@
+﻿using BE;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.DAO
+{
+    internal class DAOEstadistica : BE.ICrud<BE.Estadistica>
+    {
+        private string connstring = ""; //dir de la db
+        private SqlConnection conn;
+        private string spInsert = "AgregarEstadistica"; //storeprocedure en la db
+        private string spDelete = "EliminarEstadistica"; //storeprocedure en la db
+        private string spUpdate = "ActualizarEstadistica"; //storeprocedure en la db
+        private string spSelectAll = "EstadisticasSelectAll"; //storeprocedure en la db
+
+        private DataSet ds;
+        private SqlDataAdapter da;
+        private SqlCommand comm;
+
+        #region singleton
+        private DAOEstadistica()
+        {
+
+        }
+        private static DAOEstadistica instancia;
+        public static DAOEstadistica GetInstancia()
+        {
+            if (instancia == null)
+            {
+                instancia = new DAOEstadistica();
+            }
+            return instancia;
+        } 
+        #endregion
+        public bool Add(Estadistica entAlta)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Estadistica entAlta)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Estadistica> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Estadistica entAlta)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
